a5045208b1ec74241c2ccd8fef1e8452505a1b41
Agustín Paturlanne <aguspaturlanne@gmail.com>
2023-10-18 20:34:54 -0300

    swagger terminado

--- API-Proveedores/api.py
+++ API-Proveedores/api.py
@@ -1,12 +1,24 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request
 import jwt
 import datetime
 import secrets
 from functools import wraps
+from flask_restx import Api, Resource, fields
+
+authorizations = {
+    'Bearer Auth': {
+        'type': 'apiKey',
+        'in': 'header',
+        'name': 'Authorization'
+    },
+}
 
 app = Flask(__name__)
+api = Api(app, version='1.0', title='API de Proveedores', description='API para gestionar proveedores', authorizations=authorizations, security='Bearer Auth')
 
 SECRET_KEY = secrets.token_hex(32)
+
+ns = api.namespace('proveedor', description='Operaciones de proveedor')
 
 # Harcodeamos los datos iniciales
 datos = {
@@ -51,23 +63,43 @@ users = {
     'walter.bates': 'bpm'
 }
 
-# Ruta de autenticación
-@app.route('/login', methods=['POST'])
-def login():
-    
-    body = request.get_json()
-    username= body.get('username')
-    password= body.get('password')
+login_model = api.model('Login', {
+    'username': fields.String(required=True, description='Nombre de usuario'),
+    'password': fields.String(required=True, description='Contraseña del usuario'),
+})
 
-    if not username or not password:
-        return jsonify({'message': 'Falta nombre de usuario o contraseña'}), 401
+reserva_model = api.model('Reserva', {
+    'material': fields.String(required=True, description='Tipo de material (algodon, metal, madera o poliester)'),
+    'name': fields.String(required=True, description='Nombre del proveedor'),
+    'cantidad': fields.Integer(required=True, description='Cantidad a reservar'),
+})
 
-    if username in users and users[username] == password:
-        # Crear un token JWT con un tiempo de expiración
-        token = jwt.encode({'username': username, 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)}, SECRET_KEY, algorithm='HS256')
-        return jsonify({'token': token})
+cancelar_model = api.model('Cancelar', {
+    'material': fields.String(required=True, description='Tipo de material (algodon, metal, madera o poliester)'),
+    'name': fields.String(required=True, description='Nombre del proveedor'),
+    'cantidad': fields.Integer(required=True, description='Cantidad a cancelar'),
+    'fecha': fields.String(required=True, description='Fecha de cancelación'),
+})
 
-    return jsonify({'message': 'Nombre de usuario o contraseña incorrectos'}), 401
+@ns.route('/login')
+class LoginResource(Resource):
+    @ns.expect(login_model)
+    @ns.response(200, 'Login exitoso')
+    @ns.response(401, 'Login fallido')
+    def post(self):
+        body = request.get_json()
+        username= body.get('username')
+        password= body.get('password')
+
+        if not username or not password:
+            return {'message': 'Falta nombre de usuario o contraseña'}, 401
+
+        if username in users and users[username] == password:
+            # Crear un token JWT con un tiempo de expiración
+            token = jwt.encode({'username': username, 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)}, SECRET_KEY, algorithm='HS256')
+            return {'token': token}, 200
+
+        return {'message': 'Nombre de usuario o contraseña incorrectos'}, 401
 
 
 # Decorador personalizado para verificar el token
@@ -75,7 +107,6 @@ def verify_token(route_function):
     @wraps(route_function)
     def decorated_route(*args, **kwargs):
         token = request.headers.get('Authorization')
-        token = token.split(" ")[1]
         if token:
             try:
                 # Verificar el token JWT con la clave secreta
@@ -83,57 +114,103 @@ def verify_token(route_function):
                 # Si el token es válido, se permite el acceso a la función original
                 return route_function(*args, **kwargs)
             except jwt.ExpiredSignatureError:
-                return jsonify({"message": "Token expirado"}), 401
+                return {"message": "Token expirado"}, 401
             except jwt.InvalidTokenError:
-                return jsonify({"message": "Token inválido"}), 401
+                return {"message": "Token inválido"}, 401
         else:
-            return jsonify({"message": "Token no proporcionado"}), 401
+            return {"message": "Token no proporcionado"}, 401
     return decorated_route
 
-@app.route('/buscar/<material>/<fecha>/<cant>', methods=['GET'])
-@verify_token
-def buscar(material, fecha, cant):
-    if material in datos:
-        result = [prov for prov in datos[material] if prov["cantidad"] > 0 and prov["fecha"] <= fecha and prov["cantidad"] >= int(cant)]
-        return jsonify(result)
-    return jsonify([])
 
+def parse_and_format_date(date_string):
+    try:
+        # Intenta parsear la fecha
+        parsed_date = datetime.datetime.strptime(date_string, '%Y-%m-%d')
+        # Reformatea la fecha al formato deseado
+        return parsed_date.strftime('%Y-%m-%d')
+    except ValueError:
+        # Si hay un error, retorna None
+        return None
 
-@app.route('/reservar', methods=['POST'])
-@verify_token
-def reservar():
-    data = request.get_json()
-    material = data.get("material")
-    proveedor = data.get("name")
-    cantidad_reserva = data.get("cantidad")
-
-    for prov in datos[material]:
-        if prov["name"] == proveedor:
-            if prov["cantidad"] >= cantidad_reserva:
-                prov["cantidad"] -= cantidad_reserva
-                reservas[material].append({"name": proveedor, "cantidad": cantidad_reserva, "fecha_reserva": prov["fecha"]})
-                return jsonify({"status": "Reserva realizada con éxito"})
+#Recurso para buscar proveedores
+@ns.route('/buscar/<material>/<fecha>/<cant>')
+class BuscarResource(Resource):
+    @ns.doc(params={'material': 'Tipo de material (algodon, metal, madera o poliester)', 'fecha': 'Fecha (Año-mes-dia)', 'cant': 'Cantidad requerida'},security='Bearer Auth')
+    @verify_token
+    @ns.response(200, 'Búsqueda exitosa')
+    @ns.response(400, 'Formato de fecha inválido')
+    @ns.response(401, 'Token inválido, expirado o no proporcionado')
+    def get(self, material, fecha, cant):
+        formatted_date = parse_and_format_date(fecha)
+        print(formatted_date)
+        if not formatted_date:
+            return {"message": "Formato de fecha inválido. Por favor, use el formato 'año-mes-día'."}, 400
+        if material in datos:
+            result = [prov for prov in datos[material] if prov["cantidad"] > 0 and prov["fecha"] <= formatted_date and prov["cantidad"] >= int(cant)]
+            if result:
+                return {"message": "Búsqueda exitosa", "result": result}, 200
             else:
-                return jsonify({"status": "No hay suficiente cantidad para reservar"}), 400
-    return jsonify({"status": "Proveedor no encontrado"}), 404
+                return {"message": "No se encontraron proveedores que cumplan con los criterios"}, 404
+        else:
+            return {"message": "Material no encontrado"}, 404
 
-@app.route('/cancelar', methods=['POST'])
-@verify_token
-def cancelar():
-    data = request.json
-    material = data["material"]
-    proveedor = data["name"]
-    cantidad_cancelada = data["cantidad"]
-    fecha_cancelar = data["fecha"]
 
-    for reserva in reservas[material]:
-        if (reserva["name"] == proveedor and reserva["cantidad"] == cantidad_cancelada and reserva["fecha_reserva"] == fecha_cancelar):
-            for x in datos[material]:
-                if (x["name"] == proveedor):
-                    x["cantidad"] += cantidad_cancelada
-            reservas[material].remove(reserva)
-            return jsonify({"status": "Reserva cancelada con éxito"})
-    return jsonify({"status": "No se pudo cancelar: reserva inexistente."}), 404
+# Recurso para reservar proveedores
+@ns.route('/reservar')
+class ReservarResource(Resource):
+    @ns.doc(security='Bearer Auth')
+    @ns.expect(reserva_model)
+    @verify_token
+    @ns.response(200, 'Reserva exitosa')
+    @ns.response(400, 'No hay suficiente cantidad para reservar')
+    @ns.response(401, 'Token inválido, expirado o no proporcionado')
+    @ns.response(404, 'Proveedor no encontrado')
+    def post(self):
+        data = request.get_json()
+        material = data.get("material")
+        proveedor = data.get("name")
+        cantidad_reserva = data.get("cantidad")
+
+        for prov in datos[material]:
+            if prov["name"] == proveedor:
+                if prov["cantidad"] >= cantidad_reserva:
+                    prov["cantidad"] -= cantidad_reserva
+                    reservas[material].append({"name": proveedor, "cantidad": cantidad_reserva, "fecha_reserva": prov["fecha"]})
+                    print(reservas)
+                    return {"status": "Reserva realizada con éxito"}
+                else:
+                    return {"status": "No hay suficiente cantidad para reservar"}, 400
+        return {"status": "Proveedor no encontrado"}, 404
+
+@ns.route('/cancelar')
+class CancelarResource(Resource):
+    @ns.doc(security='Bearer Auth')
+    @ns.expect(cancelar_model)
+    @verify_token
+    @ns.response(200, 'Cancelación exitosa')
+    @ns.response(400, 'Formato de fecha inválido')
+    @ns.response(401, 'Token inválido, expirado o no proporcionado')
+    @ns.response(404, 'No se pudo cancelar: reserva inexistente.')
+    def post(self):
+        data = request.json
+        material = data["material"]
+        proveedor = data["name"]
+        cantidad_cancelada = data["cantidad"]
+        fecha_cancelar = data["fecha"]
+        formatted_date = parse_and_format_date(fecha_cancelar)
+        print(formatted_date)
+        if not formatted_date:
+            return {"message": "Formato de fecha inválido. Por favor, use el formato 'año-mes-día'."}, 400
+        for reserva in reservas[material]:
+            print(reservas)
+            if (reserva["name"] == proveedor and reserva["cantidad"] == cantidad_cancelada and reserva["fecha_reserva"] == formatted_date):
+                for x in datos[material]:
+                    if (x["name"] == proveedor):
+                        x["cantidad"] += cantidad_cancelada
+                reservas[material].remove(reserva)
+                return {"status": "Reserva cancelada con éxito"}
+        return {"status": "No se pudo cancelar: reserva inexistente."}, 404
+
 
 if __name__ == "__main__":
     app.run(port=5002,debug=True)
