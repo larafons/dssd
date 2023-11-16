3659e093bc56334f39919d596445c2c96527b3e3
Lara Fons <larafons94@gmail.com>
2023-11-16 19:19:55 -0300

    mail andando

--- API-Proveedores/api.py
+++ API-Proveedores/api.py
@@ -421,7 +421,7 @@ class ConsultarHitosResource(Resource):
     @ns.response(200, 'Hito retornado')
     @ns.response(401, 'Token inválido, expirado o no proporcionado')
     def get(self):
-        time.sleep(1)
+        time.sleep(2)
         if hitos:
             #Change and put the pop in a json where the key es "message"
             return {"mensaje": f"{hitos.pop(0)}"}, 200
@@ -440,6 +440,7 @@ class ConsultarFinResource(Resource):
         if hitos:
             return {"mensaje": "Coleccion en desarrollo"}, 402
         else:
+            hitos = ["Ya estan disponibles todos los materiales solicitados", "A partir de hoy esta disponible el espacio de fabricacion", "Todos los materiales fueron enviados a la fabrica", "Inicio el proceso de ensamblado", "Finalizo la etapa de fabricacion: coleccion terminada"]
             return {"mensaje": "Coleccion finalizada"}, 200
 
 
